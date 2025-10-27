59c32b96062821b46f4921e06adf2587a24f5127
Sandeep Kaushik <techbooz.in@gmail.com>
2025-10-27 11:42:23 +0530

    Add files via upload

---
+++ ex_session_3.py
@@ -0,0 +1,10 @@
+# 3. Calculate the area and perimeter of a rectangle. 
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("The area of the rectangle is", area)
+print("The perimeter of the rectangle is", perimeter)
---
+++ session_3.py
@@ -0,0 +1,53 @@
+# a = 45
+# b = 12.0
+# # arithematic operators in python  +, - , / , * , % , //, **
+
+# print(a+b) #addition
+ 
+# print(a-b) #subtract
+
+# print(a*b) #multiply
+
+# print(a**2)  #power
+
+# print(a/b) #divide always give float value
+
+# print(a//b) # floor division if both are integer then give integer value else float value
+
+# print(a%b) #reminder  
+# a = int(input("enter a number "))
+# b = int(input("enter another number "))
+# print(a+b)
+
+# 1. Arithmetic Operators: +, -, *, /, //, %, ** 
+# 2. Relational Operators or Comparison Operators: ==, !=, >, <, >=, <= 
+# 3. Logical Operators: and, or, not 
+# 4. Assignment Operators: =, +=, -=, *=, /=, //=, %=, **= 
+# 5. Membership operators: in, not in 
+# 6. Identity Operators: is, is not 
+# 7. Ternary Operator: <expression1> if <condition> else <expression2> 
+# print(45==46)
+# a = 45
+# a+=10
+# print(a)
+# # a = a+10
+# # print(a)
+# b = ["deepanshu",56,90,12,45]
+# print(45 not in b)
+# # c = "deepanshu"
+# # print(b is c)
+# print(not(45>9))
+# a = 44
+# b =44
+# c=44
+
+
+# print(a is b) #compare both address, value and type
+
+
+# print(a == b) #compare only values
+
+
+m = int(input("enter a number "))
+print(m)
+
